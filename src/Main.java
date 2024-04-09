4553ecaf254b9fbcb9cf1b146e660bf5fd7c9315
tjdwl0808 <tjdwl0808@gmail.com>
2024-04-09 17:31:36 +0900

    회원가입 뼈대 진행 중

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Members.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SignUp su = new SignUp();
+        su.signUp();
+    }
+}
--- src/Members/SignUp.java
+++ src/Members/SignUp.java
@@ -3,10 +3,37 @@ package Members;
 import java.util.Scanner;
 
 public class SignUp {
-    Scanner sc = new Scanner(System.in);
+    public void signUp(){
+        String inputId = inputId();
+        String inputPw = inputPw();
+        Info info = new Info(inputId, inputPw);
 
-    String inputId = sc.nextLine();
-    String inputPw = sc.nextLine();
+        System.out.printf("[회원가입] 'ID: %s, PW: %s' 가 가입되었습니다.", inputId, inputPw);
+    };
 
-    Info info = new Info(inputId, inputPw);
+    public static String inputId(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("사용할 ID를 입력하세요: ");
+        String inputId = sc.nextLine();
+
+        return inputId;
+    };
+
+    public static String inputPw(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("사용할 PW를 입력하세요: ");
+        String inputPw = sc.nextLine();
+
+        return inputPw;
+    }
+
+
+
+
+
+
+
+
 }
