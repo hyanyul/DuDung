fbcdfde547572c57aee6e75b528fdb94fc679898
tjdwl0808 <tjdwl0808@gmail.com>
2024-04-11 17:47:09 +0900

    0411

---
+++ src/Job/Job.java
@@ -0,0 +1,19 @@
+package Job;
+
+public class Job {
+    private String jobName;     // 직업명
+    private double hP;      // 체력
+    private double mP;      // 마나
+    private double cP;      // 공격력
+
+    public void skillAttack(){
+
+    }
+
+    public void skillDefence(){
+
+    }
+
+
+
+}
--- src/Main/Exe.java
+++ src/Main/Exe.java
@@ -3,14 +3,15 @@ package Main;
 import GameCharacter.*;
 import Members.*;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Exe {
     public static void main(String[] args) {
-        menu();
+        run();
     }
 
-    public static void menu(){
+    public static void run(){
         Scanner sc = new Scanner(System.in);
         SignUp su = new SignUp();       // signUp 객체 생성
         SignIn si = new SignIn();
@@ -29,16 +30,25 @@ public class Exe {
                     break;
 
                 case 2:
-                    signInArr = si.signIn();
+                    signInArr = si.signIn();        // 로그인
                     break;
 
                 case 3:
-                    cc.createChar(signInArr[0]);
-                    break;
+                    if(signInArr[0] != null || signInArr[1] != null) {         // 로그인 된 상태일 경우 캐릭터 생성
+                        cc.createChar(signInArr[0]);
+                        break;
+                    } else {        // 로그인하지 않았을 경우 캐릭터 생성 불가
+                        System.out.println("로그인한 뒤 캐릭터를 생성해주세요.");
+                        break;
+                    }
 
                 case 4:
 
+
                 case 5:
+                    signInArr = null;
+                    System.out.println("로그아웃 되었습니다.");
+                    break;
 
                 case 6:
                     System.out.println("종료되었습니다.");
